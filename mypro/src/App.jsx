0d6419403da4f50720549431963d4b07b4ef75ef
Biki Gamang <129982236+Bikigomango@users.noreply.github.com>
2024-03-08 15:32:05 +0530

    Update App.jsx
    removed my db api and url

--- mypro/src/App.jsx
+++ mypro/src/App.jsx
@@ -4,8 +4,10 @@ import { createClient } from "@supabase/supabase-js";
 import Supers from "./Components/Supers";
 
 const supabase = createClient(
-  "https://axecuodyqkzojnxmhcvy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4ZWN1b2R5cWt6b2pueG1oY3Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyNjAwNjMsImV4cCI6MjAyNDgzNjA2M30.d685EZYleyYOVv7y4Ts7LbQW64im8F0f9rVhSBRM4Dk"
+  /// supabase url
+  "https://_____________________________.supabase.co",
+  //// api key of supabase
+  "_______________________________________________________________________________________"
 );
 
 function App() {
